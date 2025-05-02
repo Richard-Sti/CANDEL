456e1ac0978f694728bd607e01b8a36e0821fc74
Richard-Sti <richard.stiskalek@protonmail.com>
2025-05-02 11:13:14 -0400

    Add basic coordinate transforms

--- candle/util.py
+++ candle/util.py
@@ -13,4 +13,37 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+import numpy as np
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
 SPEED_OF_LIGHT = 299_792.458  # km / s
+
+
+###############################################################################
+#                        Coordinate transformations                           #
+###############################################################################
+
+def radec_to_cartesian(ra, dec):
+    """
+    Convert right ascension and declination (in degrees) to unit Cartesian
+    coordinates.
+    """
+    ra_rad = np.deg2rad(ra)
+    dec_rad = np.deg2rad(dec)
+    cos_dec = np.cos(dec_rad)
+
+    x = cos_dec * np.cos(ra_rad)
+    y = cos_dec * np.sin(ra_rad)
+    z = np.sin(dec_rad)
+
+    return np.column_stack([x, y, z])
+
+
+def radec_to_galactic(ra, dec):
+    """
+    Convert right ascension and declination to galactic coordinates (all in
+    degrees).
+    """
+    c = SkyCoord(ra=ra*u.degree, dec=dec*u.degree, frame='icrs')
+    return c.galactic.l.degree, c.galactic.b.degree
