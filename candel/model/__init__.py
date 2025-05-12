a92e9a5cee2ea4defab98aafd1706aa1ac0139b4
Richard Stiskalek <richard.stiskalek@protonmail.com>
2025-05-11 22:33:43 -0400

    Add models to name2model

--- candel/model/__init__.py
+++ candel/model/__init__.py
@@ -36,6 +36,8 @@ def name2model(name):
     mapping = {
         "TFRModel": TFRModel,
         "TFRModel_DistMarg": TFRModel_DistMarg,
+        "PantheonPlusModel_DistMarg": PantheonPlusModel_DistMarg,
+        "ClustersLT_DistMarg": ClustersLT_DistMarg,
         }
 
     if name not in mapping:
