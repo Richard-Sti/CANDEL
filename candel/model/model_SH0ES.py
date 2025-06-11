249312a66a4645cca899a8a765402493a006790f
Richard-Sti <richard.stiskalek@protonmail.com>
2025-06-11 12:58:28 +0200

    Update SH0ES model

--- candel/model/model_SH0ES.py
+++ candel/model/model_SH0ES.py
@@ -13,15 +13,17 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """Turning the SH0ES framework into a forward model in JAX."""
+from abc import ABC
 
 import jax.numpy as jnp
 import numpy as np
 from jax.scipy.linalg import solve_triangular
 from numpyro import factor, plate, sample
-from numpyro.distributions import Normal, Uniform
+from numpyro.distributions import MultivariateNormal, Normal, Uniform
 
-from ..util import SPEED_OF_LIGHT, fprint, radec_to_cartesian
-from .model import JeffreysPrior, MagnitudeDistribution, sample_vector
+from ..util import (SPEED_OF_LIGHT, fprint, get_nested, load_config,
+                    radec_to_cartesian, replace_prior_with_delta)
+from .model import MagnitudeDistribution, load_priors, rsample
 
 
 def mvn_logpdf_cholesky(y, mu, L):
@@ -34,12 +36,61 @@ def mvn_logpdf_cholesky(y, mu, L):
     return -0.5 * (len(y) * jnp.log(2 * jnp.pi) + 2 * log_det + jnp.dot(z, z))
 
 
-class SH0ESModel:
-    """
-    A version of the SH0ES inference turned into a forward model.
-    """
+class BaseSH0ESModel(ABC):
 
-    def __init__(self, data):
+    def __init__(self, config_path, data):
+        config = load_config(config_path)
+
+        # Set unused parameter priors to delta functions.
+        config = self.replace_priors(config)
+        # Unpack and set the priors.
+        priors = config["model"]["priors"]
+        self.priors, self.prior_dist_name = load_priors(priors)
+
+        self.config = config
+        # Load the data, set attributes, convert to JAX arrays and do
+        # any other conversions.
+        self.set_data(data)
+
+        self.use_SNe_HF = get_nested(config, "model/use_SNe_HF", False)
+        fprint(f"use_SNe_HF set to {self.use_SNe_HF}")
+
+        self.use_Cepheid_host_redshift = get_nested(
+            config, "model/use_Cepheid_host_redshift", False)
+        fprint(f"use_Cepheid_host_redshift set to {self.use_Cepheid_host_redshift}")  # noqa
+
+        self.use_uniform_mu_host_priors = get_nested(
+            config, "model/use_uniform_mu_host_priors", True)
+        fprint(f"use_uniform_mu_host_priors set to {self.use_uniform_mu_host_priors}")  # noqa
+
+        self.use_fiducial_Cepheid_host_PV_covariance = get_nested(
+            config, "model/use_fiducial_Cepheid_host_PV_covariance", True)
+        fprint(f"use_fiducial_Cepheid_host_PV_covariance set to {self.use_fiducial_Cepheid_host_PV_covariance}")  # noqa
+
+    def replace_priors(self, config):
+        """Replace priors on parameters that are not used in the model."""
+        use_SNe_HF = get_nested(config, "model/use_SNe_HF", False)
+        use_Cepheid_host_redshift = get_nested(
+            config, "model/use_Cepheid_host_redshift", False)
+
+        if not use_SNe_HF:
+            fprint("Not using SNe_HF, replacing prior on M_B with a "
+                   "delta function.")
+            replace_prior_with_delta(config, "M_B", -19.25)
+
+        if not (use_Cepheid_host_redshift or use_SNe_HF):
+            fprint("Not using Cepheid host redshift, replacing prior on "
+                   "H0 with a delta function.")
+            replace_prior_with_delta(config, "H0", 73.04)
+
+            fprint("Not using redshift information, "
+                   "disabling Vext and sigma_v.")
+            replace_prior_with_delta(config, "Vext", [0., 0., 0.])
+            replace_prior_with_delta(config, "sigma_v", 100.0)
+
+        return config
+
+    def set_data(self, data):
         attrs_set = []
         for k, v in data.items():
             if k in ["q_names", "host_map"]:
@@ -64,25 +115,19 @@ class SH0ESModel:
 
         fprint(f"set the following attributes: {', '.join(attrs_set)}")
 
-    def sample_distance_moduli(self, use_uniform_mu_priors=False):
+    def sample_host_distmod(self):
         """Sample distance moduli for host galaxies."""
-        if use_uniform_mu_priors:
-            host_dist = Uniform(25, 40)
-            N4258_dist = Uniform(25, 35)
-            LMC_dist = Uniform(15, 23)
-            M31_dist = Uniform(20, 30)
+        if self.use_uniform_mu_host_priors:
+            dist = Uniform(15, 43)
         else:
-            host_dist = MagnitudeDistribution(25, 40)
-            N4258_dist = MagnitudeDistribution(25, 35)
-            LMC_dist = MagnitudeDistribution(15, 23)
-            M31_dist = MagnitudeDistribution(20, 30)
+            dist = MagnitudeDistribution(15, 43)
 
         with plate("hosts", self.num_hosts):
-            mu_host = sample("mu_host", host_dist)
+            mu_host = sample("mu_host", dist)
 
-        mu_N4258 = sample("mu_N4258", N4258_dist)
-        mu_LMC = sample("mu_LMC", LMC_dist)
-        mu_M31 = sample("mu_M31", M31_dist)
+        mu_N4258 = sample("mu_N4258", dist)
+        mu_LMC = sample("mu_LMC", dist)
+        mu_M31 = sample("mu_M31", dist)
 
         sample("mu_N4258_ll",
                Normal(self.mu_N4258_anchor, self.e_mu_N4258_anchor),
@@ -93,29 +138,37 @@ class SH0ESModel:
 
         return mu_host, mu_N4258, mu_LMC, mu_M31
 
-    def __call__(self, include_SN=False, use_uniform_mu_priors=False):
-        if include_SN:
-            M_B = sample("M_B", Uniform(-20, -18))
 
-        H0 = sample("H0", Uniform(0, 100))
+class SH0ESModel(BaseSH0ESModel):
+    """
+    A version of the SH0ES inference turned into a forward model.
+    """
+    def __init__(self, config_path, data):
+        super().__init__(config_path, data)
 
-        M_W = sample("M_W", Uniform(-7, -5))
-        b_W = sample("b_W", Uniform(-6, 0))
-        Z_W = sample("Z_W", Uniform(-2, 2))
+    def __call__(self, ):
+        M_B = rsample("M_B", self.priors["M_B"])
+        H0 = rsample("H0", self.priors["H0"])
+        M_W = rsample("M_W", self.priors["M_W"])
+        b_W = rsample("b_W", self.priors["b_W"])
+        Z_W = rsample("Z_W", self.priors["Z_W"])
+        Vext = rsample("Vext", self.priors["Vext"])
+        sigma_v = rsample("sigma_v", self.priors["sigma_v"])
 
         # HST and Gaia zero-point calibration of MW Cepheids.
         sample("M_W_HST", Normal(M_W, self.e_M_HST), obs=self.M_HST)
         sample("M_W_Gaia", Normal(M_W, self.e_M_Gaia), obs=self.M_Gaia)
 
-        mu_host, mu_N4258, mu_LMC, mu_M31 = self.sample_distance_moduli(
-            use_uniform_mu_priors)
+        mu_host, mu_N4258, mu_LMC, mu_M31 = self.sample_host_distmod()
 
+        # Distannce moduli for Cepheids, with corrections for LMC.
         dZP = sample("dZP", Normal(0, self.sigma_grnd))
         mu_host_cepheid = jnp.concatenate(
             [mu_host,
              jnp.array([mu_N4258, mu_LMC + dZP, mu_M31])]
             )
 
+        # Distances moduli without any corrections.
         mu_host_all = jnp.concatenate(
             [mu_host, jnp.array([mu_N4258, mu_LMC, mu_M31])]
             )
@@ -129,22 +182,25 @@ class SH0ESModel:
             mvn_logpdf_cholesky(self.mag_cepheid, mag_cepheid, self.L_Cepheid)
             )
 
-        sigma_v = sample("sigma_v", JeffreysPrior(0.1, 2500))
-        Vext = sample_vector("Vext", 0, 1000)
-        Vext_rad = jnp.sum(Vext[None, :] * self.rhat_host, axis=1)
+        if self.use_Cepheid_host_redshift:
+            Vext_rad = jnp.sum(Vext[None, :] * self.rhat_host, axis=1)
+            z_cosmo = H0 * jnp.power(10.0, mu_host / 5 - 5) / SPEED_OF_LIGHT
+            cz_pred = (1 + z_cosmo) * (1 + Vext_rad / SPEED_OF_LIGHT) - 1
+            cz_pred *= SPEED_OF_LIGHT
 
-        z_cosmo = H0 * jnp.power(10.0, mu_host / 5 - 5) / SPEED_OF_LIGHT
-        cz_pred = (1 + z_cosmo) * (1 + Vext_rad / SPEED_OF_LIGHT) - 1
-        cz_pred *= SPEED_OF_LIGHT
-
-        e_cz = jnp.sqrt(self.e2_czcmb_cepheid_host + sigma_v**2)
-
-        with plate("Cepheid_anchors_redsihift", self.num_hosts):
-            sample("cz_pred", Normal(cz_pred, e_cz),
-                   obs=self.czcmb_cepheid_host)
+            e2_cz = self.e2_czcmb_cepheid_host + sigma_v**2
+            if self.use_fiducial_Cepheid_host_PV_covariance:
+                C = self.PV_covmat_cepheid_host.at[jnp.diag_indices(len(e2_cz))].add(e2_cz)  # noqa
+                sample("cz_pred", MultivariateNormal(cz_pred, C),
+                       obs=self.czcmb_cepheid_host)
+            else:
+                e_cz = jnp.sqrt(e2_cz)
+                with plate("Cepheid_anchors_redshift", self.num_hosts):
+                    sample("cz_pred", Normal(cz_pred, e_cz),
+                           obs=self.czcmb_cepheid_host)
 
         # Distances to the host that have both supernovae and Cepheids
-        if include_SN:
+        if self.use_SNe_HF:
             mu_SN_Cepheid = self.L_SN_Cepheid_dist @ mu_host_all
             mag_SN_Cepheid = mu_SN_Cepheid + M_B
 
