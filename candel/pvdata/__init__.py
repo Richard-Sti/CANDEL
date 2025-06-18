300c40d03600a16040e95feaad87be85e45629df
Richard-Sti <richard.stiskalek@protonmail.com>
2025-06-18 16:11:31 +0200

    Remove comment

--- candel/pvdata/__init__.py
+++ candel/pvdata/__init__.py
@@ -13,7 +13,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-from .data import ( # noqa                                                      # noqa
+from .data import (                                                             # noqa
     load_CF4_data,                                                              # noqa
     load_CF4_mock,                                                              # noqa
     load_2MTF,                                                                  # noqa
