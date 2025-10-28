389efd962885c4192193c1b42be7d82dc96a511d
tariqyasin <tariq.yasin@outlook.com>
2025-10-28 13:15:43 +0000

    New mocks centred on l,b=0,0

--- scripts/cluster_runs/submit_mocks.sh
+++ scripts/cluster_runs/submit_mocks.sh
@@ -7,12 +7,7 @@ export OMPI_MCA_btl=tcp,self
 
 NMOCKS=${1:-1}
 
-DIPOLE_ONLY=""
-if [[ "$2" == "--dipole_only" ]]; then
-	DIPOLE_ONLY="--dipole_only"
-fi
-
 source ~/code/CANDEL/venv_candel/bin/activate
 
 exec python ~/code/CANDEL/scripts/cluster_runs/run_mocks.py \
-    --n_mocks_total $NMOCKS $DIPOLE_ONLY
+    --n_mocks_total $NMOCKS --output_dir results/mock_cluster_removal --nsamples 1000 --dipole_only
