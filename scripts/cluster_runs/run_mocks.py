c5224756cd761457d8266e818ed28e6b15a2b00d
tariqyasin <tariq.yasin@outlook.com>
2025-11-06 14:47:35 +0000

    Rdist test

--- scripts/cluster_runs/run_mocks.py
+++ scripts/cluster_runs/run_mocks.py
@@ -104,7 +104,7 @@ def generate_mock(nsamples, seed, field_loader, output_dir, mock_id=0):
     zeropoint_dipole_ell, zeropoint_dipole_b = radec_to_galactic(ra, dec)
 
     # Fixed distance model parameters
-    R = rng.uniform(50.0, 120.0)
+    R = rng.uniform(80.0, 130.0)
     # # p = rng.normal(2.0, 0.1)
     # # n = rng.normal(0.8, 1.2)
     p= 2.0
