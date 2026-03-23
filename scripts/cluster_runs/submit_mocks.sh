ff2f94059d9ae7561a9a83c89019b567ef8d41c4
tariqyasin <tariq.yasin@outlook.com>
2026-03-23 17:50:37 +0000

    update refs

--- scripts/cluster_runs/submit_mocks.sh
+++ scripts/cluster_runs/submit_mocks.sh
@@ -6,19 +6,14 @@ export OMPI_MCA_pml=ob1
 export OMPI_MCA_btl=tcp,self
 
 NMOCKS=${1:-1}
-RELATION=${2:-YT}
+RELATION=LTYT
 
 source ~/code/CANDEL/venv_candel/bin/activate
 
-if [ "$RELATION" = "LTYT" ]; then
-    CONFIG=~/code/CANDEL/scripts/cluster_runs/mock_cluster_LTYT_dipole.toml
-    OUTDIR=results/mock_dipole_LTYT
-else
-    CONFIG=~/code/CANDEL/scripts/cluster_runs/mock_cluster_dipole.toml
-    OUTDIR=results/mock_dipole
-fi
+CONFIG=~/code/CANDEL/scripts/cluster_runs/mock_cluster_LTYT_dipH0_nodensity2.toml
+OUTDIR=results/mock_cluster_LTYT_dipH0
 
 exec python ~/code/CANDEL/scripts/cluster_runs/run_mocks.py \
     --n_mocks_total $NMOCKS --output_dir $OUTDIR --nclusters 275 \
-    --num_samples 250 --dipole_only --which_relation $RELATION \
+    --num_samples 1000 --dipole_only --which_relation $RELATION \
     --config_dipole $CONFIG
